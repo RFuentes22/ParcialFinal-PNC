d79f4115d3cf7e008c97a1db80fef60b3a95b249
00120717 <00120717@uca.edu.sv>
2020-07-01 15:38:18 -0600

    Diseño y estilo

--- src/main/java/com/uca/ncapas/controller/MainController.java
+++ src/main/java/com/uca/ncapas/controller/MainController.java
@@ -35,7 +35,6 @@ public class MainController {
 		return mav;
 	}
 	
-	
 	//**************************CATALOGOS*********************************//
 	
 	@RequestMapping("/catalogoEscuela")
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,48 +2,57 @@
 (function ($) {
     "use strict";
 
-    
+
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
     /*==================================================================
     [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var subject = $('.validate-input input[name="subject"]');
-    var message = $('.validate-input textarea[name="message"]');
-
+    var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit',function(){
         var check = true;
 
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
-        }
-
-        if($(subject).val().trim() == ''){
-            showValidate(subject);
-            check=false;
-        }
-
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
         }
 
         return check;
     });
 
 
-    $('.validate-form .input1').each(function(){
+    $('.validate-form .input100').each(function(){
         $(this).focus(function(){
            hideValidate(this);
-       });
+        });
     });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
@@ -57,6 +66,22 @@
         $(thisAlert).removeClass('alert-validate');
     }
     
-    
+    /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function(){
+        if(showPass == 0) {
+            $(this).next('input').attr('type','text');
+            $(this).addClass('active');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type','password');
+            $(this).removeClass('active');
+            showPass = 0;
+        }
+        
+    });
+
 
 })(jQuery);
