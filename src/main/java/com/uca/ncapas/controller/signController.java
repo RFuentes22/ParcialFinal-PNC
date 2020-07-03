368779fb5f3ab969b0a1bc87c0d6e0ec553eb30d
Oscar Lovato <00127417@uca.edu.sv>
2020-07-02 22:22:40 -0600

    funcion para crear cuenta

--- src/main/java/com/uca/ncapas/controller/signController.java
+++ src/main/java/com/uca/ncapas/controller/signController.java
@@ -1,12 +1,27 @@
 package com.uca.ncapas.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.ncapas.domain.administracion.Usuario;
 
 @Controller
 public class signController {
 
 	@RequestMapping("/crearCuenta")
-	public ModelAndView
+	public ModelAndView createCount(@Valid @ModelAttribute Usuario usuario, BindingResult result ) {
+		ModelAndView mav = new ModelAndView();
+		if(result.hasErrors()) {
+			mav.setViewName("crearCuenta");
+		}else {
+			
+		}
+		
+		return mav;
+	}
 }
