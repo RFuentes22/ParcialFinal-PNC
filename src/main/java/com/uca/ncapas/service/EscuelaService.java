e3c010798218f14d99822d037b8698f86b68389a
00120717 <00120717@uca.edu.sv>
2020-07-04 15:51:23 -0600

    Update EscuelaService.java
    deleting dto

--- src/main/java/com/uca/ncapas/service/EscuelaService.java
+++ src/main/java/com/uca/ncapas/service/EscuelaService.java
@@ -1,7 +1,6 @@
 package com.uca.ncapas.service;
 
 import java.util.List;
-import com.uca.ncapas.DTO.EscuelaDTO;
 import com.uca.ncapas.domain.administracion.Municipio;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
@@ -21,8 +20,6 @@ public interface EscuelaService {
 	public List<Centro_escolar> filterNombre(String cadena) throws DataAccessException;
 
 	List<Centro_escolar> findByMun(Integer code) throws DataAccessException;
-
-	List<EscuelaDTO> findschoolByMun(String cadena) throws DataAccessException;
 	
 	//Para mostrar en tabla
 	public Page<Centro_escolar> findAll(Pageable page) throws DataAccessException;
