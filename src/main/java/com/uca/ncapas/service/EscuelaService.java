acebb7f4bf3a67e5fb0e432aa0cfc82c0e253980
00120717 <00120717@uca.edu.sv>
2020-07-04 15:32:49 -0600

    Update EscuelaService.java
    adding method

--- src/main/java/com/uca/ncapas/service/EscuelaService.java
+++ src/main/java/com/uca/ncapas/service/EscuelaService.java
@@ -1,10 +1,12 @@
 package com.uca.ncapas.service;
 
 import java.util.List;
-
 import com.uca.ncapas.DTO.EscuelaDTO;
 import com.uca.ncapas.domain.administracion.Municipio;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.uca.ncapas.domain.administracion.Centro_escolar;
 
@@ -21,5 +23,9 @@ public interface EscuelaService {
 	List<Centro_escolar> findByMun(Integer code) throws DataAccessException;
 
 	List<EscuelaDTO> findschoolByMun(String cadena) throws DataAccessException;
-
+	
+	//Para mostrar en tabla
+	public Page<Centro_escolar> findAll(Pageable page) throws DataAccessException;
+	public Long countAll();
+	public List<Centro_escolar> findAll(Sort sort);
 }
