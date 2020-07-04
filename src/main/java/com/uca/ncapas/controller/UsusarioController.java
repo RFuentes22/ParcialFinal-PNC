9c0c8b24de28619621e7b59e6ea56a164e7da055
00120717 <00120717@uca.edu.sv>
2020-07-03 20:49:56 -0600

    Update UsusarioController.java
    saveUser path

--- src/main/java/com/uca/ncapas/controller/UsusarioController.java
+++ src/main/java/com/uca/ncapas/controller/UsusarioController.java
@@ -1,5 +1,7 @@
 package com.uca.ncapas.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,24 +11,39 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.uca.ncapas.domain.administracion.Departamento;
 import com.uca.ncapas.domain.administracion.Materia;
 import com.uca.ncapas.domain.administracion.Usuario;
+import com.uca.ncapas.repositories.UsuarioRepo;
+import com.uca.ncapas.service.DepartamentoService;
 import com.uca.ncapas.service.UsuarioService;
 
 @Controller
 public class UsusarioController {
 	@Autowired
-	UsuarioService userService;
+	UsuarioRepo usuarioRepo;
 	
+	 @Autowired
+	 private DepartamentoService departamentoService;
+
+		
 	@RequestMapping("/saveUser")
-	public ModelAndView saveMateria(@Valid @ModelAttribute Usuario usuario , BindingResult result) {
+	public ModelAndView createCount(@Valid @ModelAttribute Usuario usuario, BindingResult result ) {
 		ModelAndView mav = new ModelAndView();
+		List<Departamento> departamentos = null;
+		System.out.println(usuario.getImunicipio().toString());
+		try {
+			departamentos = departamentoService.findAll();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		if(result.hasErrors()) {
-			mav.setViewName("catalogos/crearMateria");
+			mav.addObject("departamentos", departamentos);
+			mav.setViewName("crearUsuario");
 		}else {
-			userService.save(usuario);
-			mav.addObject("save", 1);
-			mav.setViewName("catalogos/catalogoUsuario");
+			usuarioRepo.save(usuario);
+			mav.setViewName("index");
 		}
 		
 		return mav;
