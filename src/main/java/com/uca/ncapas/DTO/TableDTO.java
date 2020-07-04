ab9dce57f41cd32201c8a871f29866ea238d0181
00120717 <00120717@uca.edu.sv>
2020-07-04 15:31:35 -0600

    Create TableDTO.java
    adding TableDTO

---
+++ src/main/java/com/uca/ncapas/DTO/TableDTO.java
@@ -0,0 +1,46 @@
+package com.uca.ncapas.DTO;
+
+import java.util.List;
+
+public class TableDTO {
+	private Integer draw;
+	
+	private Integer recordsTotal;
+	
+	private Integer recordsFiltered;
+	
+	private List<String[]> data;
+
+	public Integer getDraw() {
+		return draw;
+	}
+
+	public void setDraw(Integer draw) {
+		this.draw = draw;
+	}
+
+	public Integer getRecordsTotal() {
+		return recordsTotal;
+	}
+
+	public void setRecordsTotal(Integer recordsTotal) {
+		this.recordsTotal = recordsTotal;
+	}
+
+	public Integer getRecordsFiltered() {
+		return recordsFiltered;
+	}
+
+	public void setRecordsFiltered(Integer recordsFiltered) {
+		this.recordsFiltered = recordsFiltered;
+	}
+
+	public List<String[]> getData() {
+		return data;
+	}
+
+	public void setData(List<String[]> data) {
+		this.data = data;
+	}
+	
+}
