a704a07cc1a1eb8bc49f267c95e88aae5b742827
RFuentes <00062217@uca.edu.sv>
2020-07-05 12:57:24 -0600

    Valid login MateriaControllerController

--- src/main/java/com/uca/ncapas/controller/MateriaController.java
+++ src/main/java/com/uca/ncapas/controller/MateriaController.java
@@ -1,9 +1,7 @@
 package com.uca.ncapas.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
+import com.uca.ncapas.domain.administracion.Materia;
+import com.uca.ncapas.service.MateriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -12,38 +10,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.uca.ncapas.domain.administracion.Departamento;
-import com.uca.ncapas.domain.administracion.Materia;
-import com.uca.ncapas.repositories.MateriaRepo;
-import com.uca.ncapas.service.DepartamentoService;
-import com.uca.ncapas.service.MateriaService;
+import javax.validation.Valid;
 
 @Controller
 public class MateriaController {
-	
-	@Autowired
-	MateriaService materiaService;
-	
-	@RequestMapping("/saveMateria")
-	public ModelAndView saveMateria(@Valid @ModelAttribute Materia materia, BindingResult result) {
-		ModelAndView mav = new ModelAndView();
-		if(result.hasErrors()) {
-			mav.setViewName("catalogos/crearMateria");
-		}else {
-			materiaService.save(materia);
-			mav.addObject("save", 1);
-			mav.setViewName("catalogos/catalogoMateria");
-		}
-		
-		return mav;
-	}
-	
-	 @RequestMapping("/editarMateria")
-	    public ModelAndView CrearMateria(@RequestParam Integer id) {
-	        ModelAndView mav = new ModelAndView();
-	        Materia m = materiaService.findOne(id);
-	        mav.addObject("materia", m);
+
+    @Autowired
+    MateriaService materiaService;
+
+
+    @RequestMapping("/saveMateria")
+    public ModelAndView saveMateria(@Valid @ModelAttribute Materia materia, BindingResult result) {
+        ModelAndView mav = new ModelAndView();
+        if (AdminController.idusuario != 0) {
+            if (result.hasErrors()) {
+                mav.setViewName("catalogos/crearMateria");
+            } else {
+                materiaService.save(materia);
+                mav.addObject("save", 1);
+                mav.setViewName("catalogos/catalogoMateria");
+            }
+        } else mav.setViewName("index");
+        return mav;
+    }
+
+    @RequestMapping("/editarMateria")
+    public ModelAndView CrearMateria(@RequestParam Integer id) {
+        ModelAndView mav = new ModelAndView();
+        if (AdminController.idusuario != 0) {
+            Materia m = materiaService.findOne(id);
+            mav.addObject("materia", m);
             mav.setViewName("catalogos/crearMateria");
-	        return mav;
-	    }
+        } else mav.setViewName("index");
+
+        return mav;
+    }
 }
