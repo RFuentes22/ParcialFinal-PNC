bfad104aca0d676ce1cc41b6e170fb499459e91c
RFuentes <00062217@uca.edu.sv>
2020-07-05 22:22:00 -0600

    Update EstudianteRepo

--- src/main/java/com/uca/ncapas/repositories/EstudianteRepo.java
+++ src/main/java/com/uca/ncapas/repositories/EstudianteRepo.java
@@ -11,14 +11,9 @@ import com.uca.ncapas.domain.proceso_negocio.Estudiante;
 
 public interface EstudianteRepo extends JpaRepository<Estudiante, Integer>{
 	
-	@Query(value = "SELECT * FROM proceso_negocio.estudiante WHERE nombres = :snombres  ORDER BY c_estudiante \n-- #page\n",
-			  countQuery = "SELECT count(*) FROM proceso_negocio.estudiante",
-			  nativeQuery = true)
 	public Page<Estudiante> findBySnombres(Pageable page, String snombres);
 	
-	@Query(value = "SELECT * FROM proceso_negocio.estudiante WHERE nombres = :snombres  ORDER BY c_estudiante \n-- #page\n",
-			  countQuery = "SELECT count(*) FROM proceso_negocio.estudiante",
-			  nativeQuery = true)
+
 	public Page<Estudiante> findBySapellidos(Pageable page, String sapellidos);
 	
 }
