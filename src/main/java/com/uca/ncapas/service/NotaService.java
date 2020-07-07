5119b09cb327a3602fea14aaac6d574eedf118db
00120717 <00120717@uca.edu.sv>
2020-07-06 20:04:38 -0600

    Update NotaService.java
    findById

--- src/main/java/com/uca/ncapas/service/NotaService.java
+++ src/main/java/com/uca/ncapas/service/NotaService.java
@@ -3,7 +3,10 @@ package com.uca.ncapas.service;
 import java.util.List;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import com.uca.ncapas.domain.proceso_negocio.Estudiante;
 import com.uca.ncapas.domain.proceso_negocio.Nota;
 
 public interface NotaService {
@@ -14,4 +17,7 @@ public interface NotaService {
 	
 	public void save(Nota n) throws DataAccessException;
 	
+	public Page<Nota> findById(Pageable page, Integer id ) throws DataAccessException;
+
+	public Long countAll();
 }
